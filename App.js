6589c3fdd3866be8eaaefc7691ec7ba820a0706e
skmansur2810 <mansur1234143@gmail.com>
2026-03-15 15:42:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home Page</h1>} />
+    </Routes>
+  );
+}
+
+export default App;
